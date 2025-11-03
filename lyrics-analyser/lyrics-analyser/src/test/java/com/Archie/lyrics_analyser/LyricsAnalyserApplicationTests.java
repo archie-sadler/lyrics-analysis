6ea57ed1b13e59c4ebe41baa86c05cd5fde8aeaf
archie-sadler <139000631+archie-sadler@users.noreply.github.com>
2025-11-03 13:30:38 +0000

    Add files via upload

---
+++ lyrics-analyser/lyrics-analyser/src/test/java/com/Archie/lyrics_analyser/LyricsAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Archie.lyrics_analyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricsAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
